3f52b899f5caeb45ecc1ff9e7e3f6209d171df3e
Marie Godon <mariefrance.godon@gmail.com>
2016-04-12 22:37:31 +0000

    Add CSS file with styles for counter depending on how much text is remaining, updated ReadMe

--- workshop.js
+++ workshop.js
@@ -1,9 +1,21 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
 
     $("#the-textarea").keyup(function() {
-        $("#character-counter").text(140-$(this).val().length);
+
+        var remainingText = (140 - $(this).val().length)
+
+        $("#character-counter").text(remainingText);
+
+        if (remainingText > (140 * .2)) {
+            $("#character-counter").addClass("green").removeClass("yellow");
+        }
+        else if (remainingText > (140 * .1)) {
+            $("#character-counter").addClass("yellow").removeClass("red green");
+        }
+        else {
+            $("#character-counter").addClass("red").removeClass("yellow");
+        }
 
     });
-    
+
 });
